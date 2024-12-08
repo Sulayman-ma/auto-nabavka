026a25113d4ad79d9cace7579ace86372447251f
sulayman-ma <sulayman.abba@gmail.com>
2024-12-08 16:12:52 +0100

    🆕+🛠️ changes to handlers, fixed link and unlink, fixed enable and disable (but still available to all users), reworked send-ads endpoint to adapt to previous_ads field

--- backend/app/api/routes/bot.py
+++ backend/app/api/routes/bot.py
@@ -1,3 +1,6 @@
+import json
+from typing import Any
+
 from telegram import Update, Bot
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
 
@@ -5,10 +8,11 @@ from fastapi.responses import JSONResponse
 from fastapi import APIRouter, Request, BackgroundTasks, Response, status, HTTPException
 
 from app import crud
+from app.api.deps import SessionDep
 from app.core.config import settings
 from app.core.db import AsyncSessionLocal
 from app.core.security import verify_password
-from app.models import UserUpdate, UserCreate, TaskUserResponse, TaskRequest, SendAdsRequest
+from app.models import UserUpdate, UserCreate, TaskUserResponse, TaskRequest, AdsRequest
 
 
 router = APIRouter()
@@ -20,7 +24,7 @@ bot_app = Application.builder().token(BOT_TOKEN).build()
 
 """User bot commands"""
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Welcome to the Depot Scraper, please link your account and set your URL to begin. Type /help to get help remembering the commands.")
+    await update.message.reply_text("Welcome to Auto Nabavka, please link your account and set your URL to begin. Type /help to get help remembering the commands.")
 
 
 async def seturl(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -42,21 +46,26 @@ async def seturl(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not url.startswith("https://") or not "polovniautomobili" in url:
          await update.message.reply_text("Invalid URL, please provide a URL from the correct website.")
          return
+    
+    # Get chat ID from update
     chat_id = update.message.chat_id
     
     async with AsyncSessionLocal() as session:
         db_user = await crud.get_user_by_chat_id(session=session, chat_id=chat_id)
-        if db_user:
-            if db_user.is_active:
-                user_in = UserUpdate(mobili_url=url, is_task_active=True)
-                await crud.update_user(session=session, db_user=db_user, user_in=user_in)
-                await update.message.reply_text("URL set! Ads will be sent periodically, thank you for using this service.")
-                return
-            else:
-                await update.message.reply_text("Account is inactive, please contact an admin to reenable it.")
-                return
-
-    await update.message.reply_text("Account not linked, please use the /link command to link your chat to your account.")
+        # Abort if user not found
+        if not db_user:
+            await update.message.reply_text("User not found, please use /link if you have an account.")
+            return
+        # Abort if user is disabled, preventing them from changing their URL
+        if not db_user.is_active:
+            await update.message.reply_text("Account is disabled, please contact an admin to enable it.")
+            return
+        
+        """Set URL for user"""
+        user_in = UserUpdate(mobili_url=url, is_task_active=True)
+        await crud.update_user(session=session, db_user=db_user, user_in=user_in)
+        await update.message.reply_text("URL set! Ads will be sent periodically, thank you for using this service.")
+        return
 
 
 async def link(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -77,16 +86,44 @@ async def link(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     async with AsyncSessionLocal() as session:
         db_user = await crud.get_user_by_email(session=session, email=email)
-        if db_user:
-            if db_user.chat_id:
-                await update.message.reply_text("Account already linked, contact developer to unlink.")
-                return
-            else:
-                user_in = UserUpdate(chat_id=chat_id)
-                await crud.update_user(session=session, db_user=db_user, user_in=user_in)
-                await update.message.reply_text("Account linked successfully! Proceed to set your URL with the /seturl command.")
-                return
-        await update.message.reply_text("User not registered, please contact an admin for help getting registered.")
+        # Abort if user not found
+        if not db_user:
+            await update.message.reply_text("User not found.")
+            return
+        # Check if user account is tied to a chat ID already
+        if db_user.chat_id:
+            await update.message.reply_text("Your Telegram is already linked to an account, remove it wth /unlink.")
+            return
+        
+        """Link user with current chat ID"""
+        user_in = UserUpdate(chat_id=chat_id)
+        await crud.update_user(session=session, db_user=db_user, user_in=user_in)
+        await update.message.reply_text("Account linked successfully! Set your URL with the /seturl command.")
+        return
+
+
+async def unlink(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handles the /unlink command. A single argument which is the user's email is passed and this unlinks the user's account to their chat."""
+
+    if len(context.args) > 0:
+        await update.message.reply_text("The unlink command takes no arguments.")
+        return
+
+    # Get chat ID from update
+    chat_id = update.message.chat_id
+
+    async with AsyncSessionLocal() as session:
+        db_user = await crud.get_user_by_chat_id(session=session, chat_id=chat_id)
+        # Abort if user is not found.
+        if not db_user:
+            await update.message.reply_text("Chat not linked with any account, please link before unlinking.")
+            return
+
+        """Unlink account and set is_task_active to False to prevent checking in Celery"""
+        user_in = UserUpdate(chat_id=None, is_task_active=False)
+        await crud.update_user(session=session, db_user=db_user, user_in=user_in)
+        await update.message.reply_text("Account unlinked successfully! You will no longer receive notifications in this chat.")
+        return
 
 
 async def enable(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -102,18 +139,22 @@ async def enable(update: Update, context: ContextTypes.DEFAULT_TYPE):
     email = context.args[0]
     async with AsyncSessionLocal() as session:
         db_user = await crud.get_user_by_email(session=session, email=email)
-        if db_user:
-            if not db_user.is_active:
-                user_in = UserUpdate(is_active=True)
-                if db_user.mobili_url:
-                    user_in.is_task_active = True
-                await crud.update_user(session=session, db_user=db_user, user_in=user_in)
-                await update.message.reply_text("User is now active.")
-                return
-            else:
-                await update.message.reply_text("User is already active.")
-                return
-        await update.message.reply_text("User not found.")
+        # Abort if user is not found
+        if not db_user:
+            await update.message.reply_text("User not found.")
+            return
+        # Abort if user is already active
+        if db_user.is_active:
+            await update.message.reply_text("User account is already enabled.")
+            return
+
+        """Enable user"""
+        user_in = UserUpdate(is_active=True)
+        # Turn on task if user already has a URL
+        if db_user.mobili_url:
+            user_in.is_task_active = True
+        await crud.update_user(session=session, db_user=db_user, user_in=user_in)
+        await update.message.reply_text("User account enabled!")
         return
 
 
@@ -130,16 +171,20 @@ async def disable(update: Update, context: ContextTypes.DEFAULT_TYPE):
     email = context.args[0]
     async with AsyncSessionLocal() as session:
         db_user = await crud.get_user_by_email(session=session, email=email)
-        if db_user:
-            if db_user.is_active:
-                user_in = UserUpdate(is_active=False, is_task_active=False)
-                await crud.update_user(session=session, db_user=db_user, user_in=user_in)
-                await update.message.reply_text("User successfully disabled.")
-                return
-            else:
-                await update.message.reply_text("User is already inactive.")
-                return
-        await update.message.reply_text("User not found.")
+        # Abort if user is not found
+        if not db_user:
+            await update.message.reply_text("User not found.")
+            return
+        # Abort if user is already active
+        if not db_user.is_active:
+            await update.message.reply_text("User account is already disabled.")
+            return
+        
+        """Disable user"""
+        user_in = UserUpdate(is_active=False, is_task_active=False)
+        await crud.update_user(session=session, db_user=db_user, user_in=user_in)
+        await update.message.reply_text("User account disabled.")
+        return
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -180,7 +225,16 @@ async def start_registration(update: Update, context: ContextTypes.DEFAULT_TYPE)
 
 async def ask_email(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Save email to context.user_data
-    context.user_data['email'] = update.message.text.strip()
+    email = update.message.text.strip()
+    context.user_data['email'] = email
+
+    # Abort if user is already registered
+    async with AsyncSessionLocal() as session:
+        db_user = await crud.get_user_by_email(session=session, email=email)
+        if db_user:
+            await update.message.reply_text("A user with this email already exists.")
+            return ConversationHandler.END
+    
     await update.message.reply_text("User password:")
     return ASK_PASSWORD
 
@@ -214,7 +268,6 @@ async def ask_code(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await update.message.reply_text(f"Registration complete!\nEmail: {email}\nPassword: {user_password}\n")
         else:
             await update.message.reply_text("User already registerd.")
-
     return ConversationHandler.END
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -231,15 +284,15 @@ registration_handler = ConversationHandler(
     },
     fallbacks=[CommandHandler("cancel", cancel)],
 )
+
+bot_app.add_handler(registration_handler)
 bot_app.add_handler(CommandHandler("start", start))
 bot_app.add_handler(CommandHandler("seturl", seturl))
 bot_app.add_handler(CommandHandler("enable", enable))
 bot_app.add_handler(CommandHandler("disable", disable))
 bot_app.add_handler(CommandHandler("help", help))
 bot_app.add_handler(CommandHandler("link", link))
-bot_app.add_handler(registration_handler)
-
-# Handle unknown commands
+bot_app.add_handler(CommandHandler("unlink", unlink))
 bot_app.add_handler(MessageHandler(filters.COMMAND, unknown))
 
 
@@ -265,16 +318,17 @@ async def set_webhook() -> Response:
 
 
 @router.post("/queue-tasks", response_model=TaskUserResponse)
-async def queue_tasks(request: TaskRequest):
+async def queue_tasks(
+    request: TaskRequest, session: SessionDep
+) -> Any:
     # Verify if request is from one of our workers
     if request.celery_auth != settings.CELERY_AUTH:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Direct access to this resource is unavailable."
+            detail="Invalid request."
         )
 
-    async with AsyncSessionLocal() as session:
-        users = await crud.get_task_active_users(session=session)
+    users = await crud.get_task_active_users(session=session)
 
     return JSONResponse(
         content={"users": users},
@@ -283,23 +337,51 @@ async def queue_tasks(request: TaskRequest):
 
 
 @router.post("/send-ads", response_model=int, status_code=200)
-async def send_ads(request: SendAdsRequest):
+async def send_ads(
+    request: AdsRequest,
+    session: SessionDep
+) -> Any:
     # Verify if request is from one of our workers
     if request.celery_auth != settings.CELERY_AUTH:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Direct access to this resource is unavailable."
+            ddetail="Invalid request."
         )
     
+    # Bot instance
     bot = Bot(BOT_TOKEN)
+
+    # Data from request
+    ads = request.ads
     chat_id = request.chat_id
-    for ad in request.ads:
-        try:
-            await bot.send_message(chat_id=chat_id, text=ad)
-        except Exception:
-            continue
-    
+
+    # Get previous ads from requesting user
+    db_user = await crud.get_user_by_chat_id(session=session, chat_id=chat_id)
+    previous_ads: list[str] = json.loads(db_user.previous_ads)
+    user_in = UserUpdate(previous_ads=ads)
+
+    # Store ads if none found and return
+    if not previous_ads:
+        await crud.update_user(session=session, db_user=db_user, user_in=user_in)
+        return JSONResponse(
+            content={"message": f"Ads set: {chat_id}"},
+            status_code=status.HTTP_200_OK
+        )
+
+    # Send only new ads and rewrite previous ads
+    new_ads = [ad for ad in ads if ad not in previous_ads]
+    if any(new_ads):
+        for ad in new_ads:
+            try:
+                await bot.send_message(chat_id=chat_id, text=ad)
+            except Exception:
+                continue
+        await crud.update_user(session=session, db_user=db_user, user_in=user_in)
+        return JSONResponse(
+            content={"message": f"Ads sent to: {chat_id}"},
+            status_code=status.HTTP_200_OK
+        )
     return JSONResponse(
-        content={"message": f"Ads sent to: {chat_id}"},
+        content={"message": f"No new ads for: {chat_id}"},
         status_code=status.HTTP_200_OK
     )
