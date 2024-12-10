1465c17b9e72df498725ae86fa39bd8048aca36a
sulayman-ma <sulayman.abba@gmail.com>
2024-12-10 06:04:33 +0100

    🔧 redirect to dashboard from index page for now

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,11 +1,17 @@
+import { usePathname } from 'next/navigation';
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
   const token = request.cookies.get('token'); // Get the token from cookies
+  const pathname = usePathname()
 
   if (!token) {
     // Redirect to login if token is missing
     return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
   // Allow the request to proceed if token is present
